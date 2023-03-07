18e3588bf5dceeadfa3847aad629eaf237e9f4a3
Gonzalo-Riva <gon_2214@hotmail.com>
2023-03-06 22:38:07 -0300

    Initial commit

---
+++ src/dao/mongoManager/BdSessionManager.js
@@ -0,0 +1,22 @@
+const userModel = require('../models/users.model');
+
+class BdSessionManager {
+  getSession = async (email, password) => {
+    return await userModel.findOne({ email, password });
+  };
+
+  getEmail = async (email) => {
+    return await userModel.findOne(email);
+  };
+
+  createSession = async (user) => {
+    console.log(user);
+    const { firstName, lastName, email, password, rol } = user;
+    return await userModel.create({ firstName, lastName, email, password, rol });
+  };
+  UserSession = async (id) => {
+    return await userModel.findById(id);
+  };
+}
+
+module.exports = new BdSessionManager();
---
+++ src/routes/session.router.js
@@ -0,0 +1,13 @@
+const { Router } = require('express');
+const userModel = require('../dao/models/users.model');
+const viewSession = require('../controller/sessions.controller');
+const passport = require('passport');
+const { REGISTER_STRATEGY, LOGIN_STRATEGY } = require('../utils/constants');
+
+const router = Router();
+
+router.post('/register', passport.authenticate(REGISTER_STRATEGY), viewSession.sessionLogin);
+
+router.post('/login', passport.authenticate(LOGIN_STRATEGY), viewSession.loginRegister);
+
+module.exports = router;
